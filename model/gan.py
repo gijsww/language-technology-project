460dbd923e38baf34a8cd6ad5aa23ac1c3a661dd
Bick95 <daniel.bick@live.de>
2020-06-10 16:03:30 +0200

    Implemented orthogonalization

--- model/gan.py
+++ model/gan.py
@@ -56,6 +56,7 @@ class LinearGenerator(torch.nn.Module):
 
     def __init__(self, D_in, H, D_out):
         super(LinearGenerator, self).__init__()
+        self.beta = 0.001  # Default from Conneau et al. (https://github.com/facebookresearch/MUSE/blob/master/unsupervised.py#L41)
         
         self.w1 = torch.nn.Linear(D_in, D_out, bias=False)
         
@@ -70,8 +71,7 @@ class LinearGenerator(torch.nn.Module):
         Source: https://github.com/facebookresearch/MUSE/blob/3159355b93f5c3c4883808ba785ba9d18d7f5e81/src/trainer.py#L181
         """
         W = self.w1.weight.data
-        beta = 0.001  # Default from Conneau et al. (https://github.com/facebookresearch/MUSE/blob/master/unsupervised.py#L41)
-        W.copy_((1 + beta) * W - beta * W.mm(W.transpose(0, 1).mm(W)))
+        W.copy_((1 + self.beta) * W - self.beta * W.mm(W.transpose(0, 1).mm(W)))
 
         
 class Discriminator(torch.nn.Module):
