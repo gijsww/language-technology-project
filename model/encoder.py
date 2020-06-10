f62f5786d7c952c59de3deea0db67c8c0087a18b
Bick95 <daniel.bick@live.de>
2020-06-10 21:41:25 +0200

    Changed to sigmoid output for discriminator and trimmed down encoder/decoder

--- model/encoder.py
+++ model/encoder.py
@@ -6,8 +6,7 @@ class FeedForwardEncoder(torch.nn.Module):
         super(FeedForwardEncoder, self).__init__()
         self.w1 = torch.nn.Linear(D_in, H)
         self.w2 = torch.nn.Linear(H, H)
-        self.w3 = torch.nn.Linear(H, H)
-        self.w4 = torch.nn.Linear(H, D_out)
+        self.w3 = torch.nn.Linear(H, D_out)
         self.activation = torch.nn.functional.relu
         
     def forward(self, x):
@@ -16,7 +15,5 @@ class FeedForwardEncoder(torch.nn.Module):
         x = self.w2(x)
         x = self.activation(x)
         x = self.w3(x)
-        x = self.activation(x)
-        x = self.w4(x)
         return x
 
--- model/gan.py
+++ model/gan.py
@@ -49,10 +49,10 @@ class Discriminator(torch.nn.Module):
         super(Discriminator, self).__init__()
         self.w1 = torch.nn.Linear(D_in, H)
         self.w2 = torch.nn.Linear(H, H)
-        self.w3 = torch.nn.Linear(H, H)
-        self.w4 = torch.nn.Linear(H, D_out)
+        self.w3 = torch.nn.Linear(H, D_out)
         self.activation = torch.nn.functional.relu
-        self.softmax = torch.nn.Softmax(dim=1)  # Take SM along dimension=1, whereas dim=0 == Batch-size
+        #self.softmax = torch.nn.Softmax(dim=1)  # Take SM along dimension=1, whereas dim=0 == Batch-size
+        self.sigmoid = torch.nn.Sigmoid()
         
         
     def forward(self, x):
@@ -61,8 +61,6 @@ class Discriminator(torch.nn.Module):
         x = self.w2(x)
         x = self.activation(x)
         x = self.w3(x)
-        x = self.activation(x)
-        x = self.w4(x)
-        x = self.softmax(x)      # Create probability distribution over embedding being TP or FP
+        x = self.sigmoid(x)
         return x
 
