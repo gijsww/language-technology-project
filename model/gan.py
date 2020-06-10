8940fb909a18e3c153a66204846454d582d3ff06
Jorik <39156782+AI12018@users.noreply.github.com>
2020-06-10 19:16:33 +0200

    Edited GAN to Conneau style set-up

--- model/gan.py
+++ model/gan.py
@@ -4,42 +4,27 @@ from model import encoder, decoder
 
 class GAN(torch.nn.Module):
 
-    def __init__(self, D_in, H, D_out, src_languages):
+    def __init__(self, D_in, H, D_out):
         super(GAN, self).__init__()
         
-        self.generator = Generator(D_in=D_in, H=H, D_out=D_in, languages=src_languages)
+        self.generator = Generator(D_in=D_in, D_out=D_in)
         self.discriminator = Discriminator(D_in=D_in, H=H, D_out=D_out)
         
         
     def forward(self, x):
         raise Warning('Calling GAN DIRECTLY NOT IMPLEMENTED!')
         
-    
 
 class Generator(torch.nn.Module):
 
-    def __init__(self, D_in, H, D_out, languages):
+    def __init__(self, D_in, D_out):
         super(Generator, self).__init__()
-        
-        internal_dim = H  # Does not necessarily need to coincide
 
-        self.encoders = {}
-        for language in languages:
-            self.encoders[language] = encoder.FeedForwardEncoder(D_in=D_in, H=H, D_out=internal_dim)
-        
-        # Decoder
-        self.decoder = decoder.FeedForwardDecoder(D_in=internal_dim, H=H, D_out=D_out)
+        self.mapping = torch.nn.Linear(D_in, D_out, bias=False)
         
         
-    def forward(self, x, batch_language):
-        """
-            Generator's forward pass.
-        :param x: Word embedding vectors from a single language space
-        :param batch_language: Indication from which language's embedding space word embedding vectors are provided in x
-        :return: Translations of x in target language's embedding space
-        """
-        x = self.encoders[batch_language](x)  # Feed src-lang's embedding vectors through lang's respective encoder
-        x = self.decoder(x)
+    def forward(self, x):
+        x = self.mapping(x)
         return x
         
         
@@ -48,8 +33,8 @@ class Discriminator(torch.nn.Module):
     def __init__(self, D_in, H, D_out):
         super(Discriminator, self).__init__()
         self.w1 = torch.nn.Linear(D_in, H)
-        self.w2 = torch.nn.Linear(H, H)
-        self.w3 = torch.nn.Linear(H, H)
+        # self.w2 = torch.nn.Linear(H, H)
+        # self.w3 = torch.nn.Linear(H, H)
         self.w4 = torch.nn.Linear(H, D_out)
         self.activation = torch.nn.functional.relu
         self.softmax = torch.nn.Softmax(dim=1)  # Take SM along dimension=1, whereas dim=0 == Batch-size
@@ -58,11 +43,11 @@ class Discriminator(torch.nn.Module):
     def forward(self, x):
         x = self.w1(x)
         x = self.activation(x)
-        x = self.w2(x)
-        x = self.activation(x)
-        x = self.w3(x)
-        x = self.activation(x)
+        # x = self.w2(x)
+        # x = self.activation(x)
+        # x = self.w3(x)
+        # x = self.activation(x)
         x = self.w4(x)
-        x = self.softmax(x)      # Create probability distribution over embedding being TP or FP
+        x = self.sigmoid(x)      # Create probability distribution over embedding being TP or FP
         return x
 
