65a102b32f6a4e364c7959394f0f8f34788d7411
Bick95 <daniel.bick@live.de>
2020-06-11 17:16:16 +0200

    Proofs of concept done for custom implementations

--- model/gan.py
+++ model/gan.py
@@ -4,10 +4,21 @@ from model import encoder, decoder
 
 class GAN(torch.nn.Module):
 
-    def __init__(self, D_in, H, D_out, src_languages):
+    def __init__(self, D_in, H, D_out, src_languages, mode='nonlinear'):
         super(GAN, self).__init__()
         
-        self.generator = Generator(D_in=D_in, H=H, D_out=D_in, languages=src_languages)
+        # Construct generator
+        if mode is 'nonlinear':
+            print('Created: Nonlinear GAN.')
+            self.generator = Generator(D_in=D_in, H=H, D_out=D_in, languages=src_languages)
+        
+        elif mode is 'linear':
+            print('Created: Nonlinear GAN.')
+            self.generator = LinearGenerator(D_in=D_in, H=H, D_out=D_in)
+        
+        else:
+            raise Warning('Mode not implemented!')
+
         self.discriminator = Discriminator(D_in=D_in, D_out=D_out)
         
         
@@ -42,7 +53,32 @@ class Generator(torch.nn.Module):
         x = self.decoder(x)
         return x
         
+
+
+class LinearGenerator(torch.nn.Module):
+
+    def __init__(self, D_in, H, D_out):
+        super(LinearGenerator, self).__init__()
+        self.beta = 0.001  # Default from Conneau et al. (https://github.com/facebookresearch/MUSE/blob/master/unsupervised.py#L41)
         
+        self.w1 = torch.nn.Linear(D_in, D_out, bias=False)
+        
+        
+    def forward(self, x):
+        x = self.w1(x)
+        return x
+
+
+    def orthogonalize(self):
+        """
+        Orthogonalize the linear mapping (iteratively).
+        Source: https://github.com/facebookresearch/MUSE/blob/3159355b93f5c3c4883808ba785ba9d18d7f5e81/src/trainer.py#L181
+        """
+        W = self.w1.weight.data
+        W.copy_((1 + self.beta) * W - self.beta * W.mm(W.transpose(0, 1).mm(W)))
+
+
+
 class Discriminator(torch.nn.Module):
 
     def __init__(self, D_in=300, H=2048, D_out=1):
